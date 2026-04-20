bee5d8b64c882e21aaf19cd9d7cbedfb79ddde9d
Melissa4Campbell <Melissadc136@gmail.com>
2026-04-20 07:14:27 -0400

    Add practice file with library exercise

---
+++ practice.py
@@ -0,0 +1,22 @@
+# First Practice: Create variables and have them print out
+# Program meant for library to store information on books and print it out
+
+# Format will be:
+# Book title
+# Author Name
+# Page numbers
+# Check out status (True/False)
+
+print ("Dune Book Details:")
+
+Book_Title = "Dune"
+Author = "Frank Herbert"
+Pages = 412
+Check_Status = False
+
+print("Title:", Book_Title)
+print("Author:", Author)
+print("Pages:", Pages)
+print("Checked out:",Check_Status)
+
+
